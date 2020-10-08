e4da03c26d6aa86550785fac88db4a739525b5f2
chaohuichen <chenpeter0821@gmail.com>
2020-10-07 21:21:17 -0400

    set up the eslintrc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Keep Up</h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
