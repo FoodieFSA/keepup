d3912fb4370f6962820d9efe99968b4420bdeeb6
mkzheng972 <markzheng27@gmail.com>
2020-10-08 22:12:12 -0400

    worked on signup and login formvvia formik and yup also added materialui styling to forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Footer from './Components/Footer';
+import SignUp from './Components/SignUp';
+import LogIn from './Components/Login';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Keep Up</h1>
+        <SignUp />
       </header>
       <Footer />
     </div>
