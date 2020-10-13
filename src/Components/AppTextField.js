50975508e95eda84fe969d9251cbb6bbd60b5036
chaohuichen <chenpeter0821@gmail.com>
2020-10-13 17:03:58 -0400

    clean up

--- src/Components/AppTextField.js
+++ src/Components/AppTextField.js
@@ -11,12 +11,12 @@ const useStyles = makeStyles(() => ({
   }
 }))
 
-export default (props) => {
+const AppTextField = (props) => {
   const classes = useStyles()
   return (
     <div style={{ display: 'flex', flexDirection: 'column', color: 'red' }}>
       <TextField
-        id='standard-basic'
+        id="standard-basic"
         className={classes.root}
         InputLabelProps={{ className: classes.input }}
         InputProps={{ className: classes.input }}
@@ -26,8 +26,10 @@ export default (props) => {
         value={props.values[props.name]}
         onChange={props.handleChange}
       />
-      {
-        props.name in props.errors ? SetFormMessage(props.errors[props.name]) : ' '
-      }
-    </div>)
+      {props.name in props.errors
+        ? SetFormMessage(props.errors[props.name])
+        : ' '}
+    </div>
+  )
 }
+export default AppTextField
