9605ab626a587b625a79567d0c026b8bd77c672d
Thomas Zhang <thomaszhang97@gmail.com>
2020-10-13 00:22:16 -0400

    sticky footer

--- src/App.js
+++ src/App.js
@@ -24,11 +24,13 @@ function App () {
     <Router>
       <div className='App'>
         <NavBar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/signup'><SignUp /></Route>
-          <Route path='/login'><LogIn /></Route>
-        </Switch>
+        <div id="main">
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/signup'><SignUp /></Route>
+            <Route path='/login'><LogIn /></Route>
+          </Switch>
+        </div>
         <Footer />
       </div>
     </Router>
