75bfeebc2ac3cc6e6b186a26dda78f8a98dc303a
chaohuichen <chenpeter0821@gmail.com>
2020-10-14 13:22:13 -0400

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './Styles/App.css'
 import Footer from './Components/Footer'
 import NavBar from './Components/NavBar'
-import api from '../Api'
 
 import Routes from './routes'
 function App () {
-  const test = async () => {
-    const response = await api.post('auth/registerUser', {
-      Email: 'abc@abc.com',
-      Password: '12345678'
-    })
-    console.log(response)
-  }
-  useEffect(() => {
-    test()
-  }, [])
   return (
-    <div className="App">
+    <div className='App'>
       <NavBar />
-      <div id="main">
+      <div id='main'>
         <Routes />
       </div>
       <Footer />
