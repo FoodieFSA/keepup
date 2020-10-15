aa960d6ec532e9f9c235f7fe5783112ec40a6ff6
chaohuichen <chenpeter0821@gmail.com>
2020-10-15 13:21:08 -0400

    added redux

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App () {
   return (
     <div className='App'>
       <NavBar />
-      <div id='main'>
-        <Routes />
-      </div>
+      <Routes />
       <Footer />
     </div>
   )
