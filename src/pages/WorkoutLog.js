e2d20d62d876f1c19f828f2eb8e5889240f433e5
Thomas Zhang <thomaszhang97@gmail.com>
2020-10-20 22:51:14 -0400

    Rendered a simple table, need to fix icon bug

--- src/pages/WorkoutLog.js
+++ src/pages/WorkoutLog.js
@@ -1,6 +1,4 @@
-import React from 'react'
 import MaterialTable from 'material-table'
-import { AddBox, ArrowDownward } from "@material-ui/icons";
 
 const createData = (
   bodyPart,
@@ -9,21 +7,21 @@ const createData = (
   exereciseWeight,
   exerciseRep
 ) => {
-  return {bodyPart, exerciseName, exerciseSet, exereciseWeight, exerciseRep}
+  return { bodyPart, exerciseName, exerciseSet, exereciseWeight, exerciseRep }
 }
 
 const data = [
   createData('Chest', 'Flat Bench Press', 4, 135, 8),
   createData('Back', 'Deadlift', 5, 225, 5),
-  createData('Shoulders', 'Lateral Raise', 4, 15, 10),
+  createData('Shoulders', 'Lateral Raise', 4, 15, 10)
 ]
 
 const columns = [
-  {title: 'Body Part', field: 'bodyPart'},
-  {title: 'Exercise Name', field: 'exerciseName'},
-  {title: 'Exercise Set', field: 'exerciseSet'},
-  {title: 'Exercise Weight', field: 'exereciseWeight'},
-  {title: 'Exercise Rep', field: 'exerciseRep'},
+  { title: 'Body Part', field: 'bodyPart' },
+  { title: 'Exercise Name', field: 'exerciseName' },
+  { title: 'Exercise Set', field: 'exerciseSet' },
+  { title: 'Exercise Weight', field: 'exereciseWeight' },
+  { title: 'Exercise Rep', field: 'exerciseRep' }
 ]
 
 const WorkoutLog = () => {
