0fe60008ff25791c4b5a2225fade9c8582a4098f
YanGao85 <ing.yangao@gmail.com>
2020-10-21 20:36:27 -0400

    Feature- Added 404 Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import Home from './Home'
 import LogIn from './LogIn'
 import SignUp from './SignUp'
+import Error from './Error'
 
-export {
-  Home,
-  LogIn,
-  SignUp
-}
+export { Home, LogIn, SignUp, Error }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Route, Switch } from 'react-router-dom'
-import { SignUp, LogIn, Home } from './pages'
+import { SignUp, LogIn, Home, Error } from './pages'
 
 function Router () {
   return (
     <Switch>
-      <Route path='/signup' component={SignUp} />
-      <Route path='/login' component={LogIn} />
-      <Route path='/' exact component={Home} />
+      <Route path="/signup" exact component={SignUp} />
+      <Route path="/login" exact component={LogIn} />
+      <Route path="/" exact component={Home} />
+      <Route component={Error} />
     </Switch>
   )
 }
