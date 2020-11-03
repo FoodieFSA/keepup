313b856f18c74187478fb10bbdc8ddcdb171bf08
Thomas <57773441+thomaszhangg@users.noreply.github.com>
2020-11-03 17:48:35 -0500

    Update src/pages/WorkoutLog.js
    Co-authored-by: PETER(ChaoHui) CHEN <chenpeter0821@gmail.com>

--- src/pages/WorkoutLog.js
+++ src/pages/WorkoutLog.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import {Button} from '@material-ui/core'
 import produce from 'immer'
 import SingleExercise from '../Components/SingleExercise'
